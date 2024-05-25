85b120f71484c468a908f47cce1a9d14c314c492
Loay Mohamed <loaymohamed1999@gmail.com>
2024-05-25 10:39:19 +0300

    add maxPayloadSize validation check on parser class

--- Parser/Parser.h
+++ Parser/Parser.h
@@ -3,21 +3,23 @@
 #include "../Packets/IPPacket.h"
 #include "../Packets/ARPPacket.h"
 
-class ApplicationManager;	//forward class declaration to prevent circuar dependency
+class ApplicationManager; // forward class declaration to prevent circuar dependency
 
-class Parser {
+class Parser
+{
 private:
-	int destinationMacSize = 12;// 6 Bytes (1 Byte = 2 Hexa)
-	int sourceMacSize = 12;		// 6 Bytes
-	int etherTypeSize = 4;		// 2 Bytes
-	int checksumSize = 8;		// 4 Bytes
-	int minPayloadSize = 92;	// 46 Bytes (payload should be [46-1500] Bytes)
+	int destinationMacSize = 12; // 6 Bytes (1 Byte = 2 Hexa)
+	int sourceMacSize = 12;		 // 6 Bytes
+	int etherTypeSize = 4;		 // 2 Bytes
+	int checksumSize = 8;		 // 4 Bytes
+	int minPayloadSize = 92;	 // 46 Bytes (payload should be [46-1500] Bytes)
+	int maxPayloadSize = 3000;	 // 1500 Bytes (payload should be [46-1500] Bytes)
 
 protected:
-	ApplicationManager* pManager;	//Parser needs AppMngr to do their job
+	ApplicationManager *pManager; // Parser needs AppMngr to do their job
 
 public:
-	Parser(ApplicationManager* pApp);
+	Parser(ApplicationManager *pApp);
 
 	int GetMetaDataSize(); // Calculate the sizes of all except payload
 
